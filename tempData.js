1e71f33908280743181831a9c28730abf73a964a
nathan <nathan@csdsupports-Mac-mini.local>
2022-04-14 10:42:28 +0800

    My first commit

---
+++ tempData.js
@@ -0,0 +1,74 @@
+//Temporary Data until we connect with Firebase
+
+export default tempData = [
+    {
+        id: 1,
+        name: "Plan a trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            },
+        ],
+    },
+    {
+        id: 2,
+        name: "Errands",
+        color: "#8022D9",
+        todos: [
+            {
+                title: "Buy Milk",
+                completed: true
+            },
+            {
+                title: "Buy Eggs",
+                completed: true
+            },
+            {
+                title: "Pass Bills",
+                completed: true
+            },
+            {
+                title: "Walk the dog",
+                completed: false
+            },
+        ],
+    },
+    {
+        id: 3,
+        name: "Birthday Party",
+        color: "#5958D9",
+        todos: [
+            {
+                title: "Get Balloons",
+                completed: false
+            },
+            {
+                title: "Send Invitations",
+                completed: false
+            },
+            {
+                title: "Make Dinner Reservations",
+                completed: true
+            },
+            {
+                title: "Decorate House",
+                completed: false
+            },
+        ],
+    },
+
+]
